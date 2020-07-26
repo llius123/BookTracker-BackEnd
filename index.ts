e5a0ea4829662a0d61362284a0eac2404eb40450
Jesus Berisa Nuñez <33457979+llius123@users.noreply.github.com>
2020-07-26 12:36:53 +0200

    test

--- index.ts
+++ index.ts
@@ -19,9 +19,9 @@ app.use(cookieParser());
 /**
  * Creo la conexion de la base de datos con typeorm
  */
-createConnection().then(connection => {
-	console.log("Conectado a mysql: ", connection.isConnected);
-});
+// createConnection().then(connection => {
+// 	console.log("Conectado a mysql: ", connection.isConnected);
+// });
 
 const usuario = new User();
 usuario.User(app);
@@ -34,23 +34,26 @@ app.get("/api/", (req: Request, res: Response) => {
 });
 
 app.get("/api/test", async (req: Request, res: Response) => {
-	https.get("https://openlibrary.org/search.json?q=los%20juegos%20del%20hambre&limit=10&mode=everything", resp => {
-		let data = "";
+	https.get(
+		"https://openlibrary.org/search.json?q=los%20juegos%20del%20hambre&limit=10&mode=everything",
+		(resp) => {
+			let data = "";
 
-		// A chunk of data has been recieved.
-		resp.on("data", chunk => {
-			data += chunk;
-		});
+			// A chunk of data has been recieved.
+			resp.on("data", (chunk) => {
+				data += chunk;
+			});
 
-		resp.on("error", error => {
-			res.send(error);
-		});
-		// The whole response has been received. Print out the result.
-		resp.on("end", () => {
-			// console.log(data);
-			res.send(JSON.parse(data));
-		});
-	});
+			resp.on("error", (error) => {
+				res.send(error);
+			});
+			// The whole response has been received. Print out the result.
+			resp.on("end", () => {
+				// console.log(data);
+				res.send(JSON.parse(data));
+			});
+		}
+	);
 });
 
 app.listen(3000, () => console.log("Server express running 3000"));
